07b2062eeff9b08cb66f4cefe71893e35c165e21
t0dysseus <mdxppbzwdt@privaterelay.appleid.com>
2026-06-05 08:02:46 +0200

    Update data.js

--- js/data.js
+++ js/data.js
@@ -312,7 +312,9 @@ Aus unseren Interviews hat sich ergegebe, dass die Azubis im Metallbereich es sc
     title: "Zielbild 2026",
     kategorie: ["Physische Erfahrung"],
     status: "pausiert",
-    text: "Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.",
+    text: "Wenn wir dem CJD mit einen vergleichenden Flyer zum „Zielbild CJD 2026“-Flyer konfrontieren, dann schaffen wir es, dem CJD eine Offenlegung seiner einst selbst gesteckten Ziele vor Augen zu führen, weil wir damit eine klare Gegenüberstellungen erzeugen, welche Ziele erreicht oder eben nicht erreicht wurden. Das CJD brachte in der Vergangenheit einen „Zeilbild CJD 2026“-Flyer heraus, welches eine Weiterentwicklung zum vorherigen Zielbild darstellen sollte.
+Wir erarbeiten einen Flyer, der vom (optischen) Stil her auf dem des Originals des CJD's aufbaut und bringen inhaltlich jedoch die erreichten oder aber eben nicht erreichten Ziele des Leitbildes hervor.
+",
     images: ["images/zielbild_01.jpg"]
   },
 
